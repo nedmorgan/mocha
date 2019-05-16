db32452bcb4b7c15b5b885b643ef02a17ac97892
nedmorgan <nmorgan1987@gmail.com>
2019-05-16 15:07:48 -0400

    created a test for reversing a word

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ module.exports = {
   },
   addNumbers: function(value1, value2) {
     return value1 + value2
+  },
+  reverseWord: function(word) {
+    return word
+      .split('')
+      .reverse()
+      .join('')
   }
 }
--- test/appTest.js
+++ test/appTest.js
@@ -6,22 +6,32 @@ const app = require('../app')
 //Results
 let sayHelloResult = app.sayHello()
 let addNumbersResult = app.addNumbers(5, 5)
+let reverseWordResult = app.reverseWord('walter')
 
 describe('App', function() {
-  it('sayHello should return hello', function() {
-    // let result = app.sayHello()
-    assert.equal(sayHelloResult, 'hello')
+  describe('sayHello()', function() {
+    it('sayHello should return hello', function() {
+      // let result = app.sayHello()
+      assert.equal(sayHelloResult, 'hello')
+    })
+    it('sayHello should return type string', function() {
+      // let result = app.sayHello()
+      assert.typeOf(sayHelloResult, 'string')
+    })
   })
-  it('sayHello should return type string', function() {
-    // let result = app.sayHello()
-    assert.typeOf(sayHelloResult, 'string')
+  describe('addNumbers()', function() {
+    it('addNumbers should be above 5', function() {
+      // let result = app.addNumbers(5, 5)
+      assert.isAbove(addNumbersResult, 5)
+    })
+    it('addNumber should return type number', function() {
+      // let result = app.addNumbers(5, 5)
+      assert.typeOf(addNumbersResult, 'number')
+    })
   })
-  it('addNumbers should be above 5', function() {
-    // let result = app.addNumbers(5, 5)
-    assert.isAbove(addNumbersResult, 5)
-  })
-  it('addNumber should return type number', function() {
-    // let result = app.addNumbers(5, 5)
-    assert.typeOf(addNumbersResult, 'number')
+  describe('reverseWord()', function() {
+    it('reverseWord should reverse a word', function() {
+      assert.equal(reverseWordResult, 'retlaw')
+    })
   })
 })
